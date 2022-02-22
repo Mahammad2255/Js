675ce6ebe71879054fc42fc4047367831a460a2a
Mahammad2255 <94452496+Mahammad2255@users.noreply.github.com>
2022-02-22 14:47:44 -0800

    Add files via upload

---
+++ js-task-1/app.js
@@ -0,0 +1,12 @@
+
+
+
+
+function Sil(){
+    
+    var value = document.getElementById('inp').value;
+    value = value.substr(0, value.length - 1);
+    document.getElementById('inp').value = value;
+    
+    
+}
